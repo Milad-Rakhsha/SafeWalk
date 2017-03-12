2587baa3d823a903f2fd50fcb1ad80eb81ba0a0a
patri <patri@DESKTOP-KNR76DD>
2017-03-12 11:31:17 -0500

    Added a RESTful notification button

--- FrontEnd/Hopper Rides/Hopper_Rides/MainPage.xaml.cs
+++ FrontEnd/Hopper Rides/Hopper_Rides/MainPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -23,6 +26,20 @@ namespace Hopper_Rides
             //    Text = "It's Clicked"           
             //};
             App.Current.MainPage = new MapPage();
+        }
+
+        async void sendRequest(Object sender, EventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                string json = "{ \"Something\": \"Hello\" }";
+                
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("http://thehopper.azurewebsites.net/", content);
+                var responseString = await response.Content.ReadAsStringAsync();
+                Debug.WriteLine(responseString);
+
+            }
         }
 	}
 }
