57d96fe569dec16300d9c184b4cdabd2ea0f90a1
patrick <patrick.swailes@gmail.com>
2017-03-14 18:22:35 -0500

    Added a button that calls a new content page - Note that this does not create a navigation page.  This means you cannot go "back" to the previous page without redrawing through code.
    Also added in the button that sends JSON to our server.
    
    Needed to add some references in to get IOS to work.
    
    Tested on both devices.

--- FrontEnd/Hopper Rides/Hopper_Rides/MainPage.xaml.cs
+++ FrontEnd/Hopper Rides/Hopper_Rides/MainPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -12,6 +15,30 @@ namespace Hopper_Rides
 		public MainPage()
 		{
 			InitializeComponent();
+		}
+
+		void onLogin(Object sender, EventArgs e)
+         {
+             //OK need to replace this with a call to the new map screen.
+             Content = new Label{
+                Text = "It's Clicked",
+				VerticalTextAlignment = TextAlignment.Center,
+				HorizontalTextAlignment = TextAlignment.Center           
+             };
+         }
+
+		async void sendRequest(Object sender, EventArgs e)
+		{
+			using (var client = new HttpClient())
+			{
+				string json = "{ \"Something\": \"Hello\" }";
+
+				var content = new StringContent(json, Encoding.UTF8, "application/json");
+				var response = await client.PostAsync("http://thehopper.azurewebsites.net/", content);
+				var responseString = await response.Content.ReadAsStringAsync();
+				Debug.WriteLine(responseString);
+
+			}
 		}
 	}
 }
