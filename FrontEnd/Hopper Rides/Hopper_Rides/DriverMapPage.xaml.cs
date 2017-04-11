e2ba586292f02aeaea99ac141dc1a8d0bdb2e969
patri <patri@DESKTOP-KNR76DD>
2017-04-11 18:57:28 -0500

    Removed an app.config an invalid reference in the portable Hopper_Rides.csproj folder.
    Added some logic to the DriverMapPage.  Will display some pins around the campus now.
    
    Added methods to set up an array of Pin objects and one that will be used to update the pins by talking to the backend.

--- FrontEnd/Hopper Rides/Hopper_Rides/DriverMapPage.xaml.cs
+++ FrontEnd/Hopper Rides/Hopper_Rides/DriverMapPage.xaml.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Xamarin.Forms.GoogleMaps;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace Hopper_Rides
 {
@@ -37,12 +40,62 @@ namespace Hopper_Rides
 				HorizontalTextAlignment = TextAlignment.Center
 			};
 
+			double[] xC = new double[] { 43.0765374, 43.071788, 43.072705, 43.074792, 43.074893, 43.074707, 43.069408 };
+			double[] yC = new double[] { -89.399286, -89.407911, -89.399882, -89.38437, -89.388267, -89.395909, -89.396661 };
+			string[] descrip = new string[] { "Union", "Union South", "Vilas hall", "Capitol", "Overture Center", "Brats", "Kohl Center" };
+			Pin[] pintime = drawPins(7, xC, yC, descrip);
+			for (int i = 0; i < pintime.Length; i++)
+			{
+				map.Pins.Add(pintime[i]);
+			}
+
+			//getPinData();
+
 			map.Pins.Add(pin);
-			//Not yet sure what this part does...
+			
 			var stack = new StackLayout { Spacing = 0 };
 			stack.Children.Add(name);
 			stack.Children.Add(map);
 			Content = stack;
+		}
+		
+		async void getPinData()
+		{
+			using (var client = new HttpClient())
+			{
+
+				client.DefaultRequestHeaders.Add("ZUMO-API-VERSION", "2.0.0");
+				
+				var response = await client.GetAsync("http://thehopper.azurewebsites.net/api/ActiveRequests");
+				//This will be an IQueryable object
+				//var responseString = await response.Content.ReadAsStringAsync();
+				//Debug.WriteLine(responseString);
+				//The following will convert the json to an actual rider object
+				//Hopper_Rides.Models.Rider rider = JsonConvert.DeserializeObject<Hopper_Rides.Models.Rider>(responseString);
+				/*
+				Debug.WriteLine(rider.FirstName);
+				Debug.WriteLine(rider.LastName);
+				Debug.WriteLine(rider.PhoneNumber);
+				Debug.WriteLine(rider.Email);
+				*/
+				
+
+			}
+		}
+		
+		
+		Pin[] drawPins(int num, double[] xCords, double[] yCords, string[] desc)
+		{
+			Pin[] pins = new Pin[num];
+			for (int i = 0; i < num; i++)
+			{
+				Position tempPos = new Position(xCords[i], yCords[i]);
+				Pin tempPin = new Pin();
+				tempPin.Position = tempPos;
+				tempPin.Label = desc[i];
+				pins[i] = tempPin;
+			}
+			return pins;
 		}
 	}
 }
