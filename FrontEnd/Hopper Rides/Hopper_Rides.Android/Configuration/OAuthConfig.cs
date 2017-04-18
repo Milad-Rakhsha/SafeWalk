d2307c28f95f5d2dcd44afc282dc1f0794ab4473
Milad Rakhsha <rakhsha@wisc.edu>
2017-04-17 19:14:59 -0500

    Fix the OauthConfig file from the previous commit

--- FrontEnd/Hopper Rides/Hopper_Rides.Android/Configuration/OAuthConfig.cs
+++ FrontEnd/Hopper Rides/Hopper_Rides.Android/Configuration/OAuthConfig.cs
@@ -1,25 +1,47 @@
 ﻿using System;
 using Xamarin.Forms;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net.Http;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
-namespace DevEnvExeLogin
+
+namespace Hopper_Rides
 {
-   public class OAuthConfig
-    {
-        
-        public static HomePage _HomePage;
-        static NavigationPage _NavigationPage;
-        public static UserDetails User;
+	public class OAuthConfig
+	{
 
-        public static Action SuccessfulLoginAction
-        {
-            get
-            {
-                return new Action(() =>
-                {
-                    _NavigationPage.Navigation.PushModalAsync(_HomePage);
-                });
-            }
-        }
+		public static MainPage my_MainPage = new MainPage();
+		static NavigationPage _NavigationPage = new NavigationPage();
+		public static Models.Rider User;
 
-    }
+		public static void PostRider()
+		{
+			using (var client = new HttpClient())
+			{
+				//Serialize it
+				string ser_obj = JsonConvert.SerializeObject(User);
+				var content_post = new StringContent(ser_obj, Encoding.UTF8, "text/json");
+				//post it to the proper table
+				var response_post = client.PostAsync("http://thehopper.azurewebsites.net/api/riders", content_post);
+				response_post.Wait();
+				Debug.WriteLine(ser_obj);
+				Debug.WriteLine("Posting a new User to the database");
+			}
+		}
+
+
+		public static Action SuccessfulLoginAction
+		{
+			get
+			{
+				return new Action(() =>
+				{
+					_NavigationPage.Navigation.PopAsync();
+				});
+			}
+		}
+
+	}
 }
